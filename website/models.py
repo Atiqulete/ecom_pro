d901035c00137913aedfc6917041d2dc9dd1e785
unknown <atiqulete@gmail.com>
2024-07-14 09:34:25 +0600

    V_1.0

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactInfo(models.Model):
+    Name = models.CharField(max_length=300)
+    Email = models.EmailField()
+    Phone_Number = models.CharField(max_length=13)
+    Subject = models.CharField(max_length=450)
+    Message = models.TextField()
+
+    class Meta:
+       verbose_name_plural = "Contact-Infos" 
+    def __str__(self):
+        return self.Name
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from website.views import index
+from website.views import index,contact
 
 urlpatterns = [
     path('',index,name='index'),
+    path('contact/',contact,name='contact'),
 ]
