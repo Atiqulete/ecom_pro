dd6be761422edf065fdfea4648d20e4c2307fa5e
Atiqulete <atiqulete@gmail.com>
2024-07-17 21:38:56 +0600

    loging and logout

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from user_app.views import register
+from user_app.views import register,login_page,logout_view
 
 urlpatterns = [
     path('register/',register,name='register'),
+    path('login_page/',login_page,name='login_page'),
+    path('logout/',logout_view,name='logout_view'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,15 @@
+# Create your register page here.
 from django.shortcuts import render,redirect
 from user_app.forms import RegisterForm
 
-# Create your views here.
+# Create your login page here.
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect, HttpResponse
+
+from django.contrib.auth import logout
+
+
+# Create your register code here.
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -11,3 +19,22 @@ def register(request):
     else:
         form=RegisterForm()
     return render(request,'user_app/register.html',{'form':form})
+
+# Create your login code here.
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('password')
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('contact')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'user_app/login.html')
+
+# Create your logout code here.
+def logout_view(request):
+    logout(request)
+    return redirect('/')
